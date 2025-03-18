65db3bbb5aaa1826d4fed9f092d36493c2f58637
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2025-03-18 10:28:00 +0100

    product rerouting

--- MakersMarkt/MakersMarkt/LoginPage.xaml.cs
+++ MakersMarkt/MakersMarkt/LoginPage.xaml.cs
@@ -64,7 +64,7 @@ namespace MakersMarkt
                             break;
 
                         case 2:
-                            this.Frame.Navigate(typeof(ProductPage));
+                            this.Frame.Navigate(typeof(User.UserPage));
                             break;
 
                         case 3:
